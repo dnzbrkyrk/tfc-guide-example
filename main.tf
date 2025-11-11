ea1e0c433de9c4b47f40f517b90cb422ef981f18
dnzbrkyrk <denizberkyurek@yahoo.com>
2025-11-11 22:43:45 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
-data "azurerm_resource_group" "example" {
-  name     = "az204webapp5454_group"
+data "azurerm_linux_web_app" "az204webapp5454" {
+  name                = "existing"
+  resource_group_name = "az204webapp5454_group"
+}
+
+output "id" {
+  value = data.azurerm_linux_web_app.example.id
 }
